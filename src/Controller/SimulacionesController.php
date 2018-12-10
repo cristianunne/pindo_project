6dc1b9058a0e4c26573e9d74e81ec45a704b5e71
CRISTIAN\Cristian <cristianunne@gmail.com>
2018-12-10 10:49:48 -0300

    Update SimulacionesController.php

--- src/Controller/SimulacionesController.php
+++ src/Controller/SimulacionesController.php
@@ -542,13 +542,11 @@ class SimulacionesController extends AppController
             $spreadsheet->getActiveSheet()->getColumnDimension($columnID)
                 ->setAutoSize(true);
         }
-
-
+        
         $path = WWW_ROOT . '/files/simulacion.xlsx';
 
         $writer = new Xlsx($spreadsheet);
         $writer->save($path);
-
 
         $this->response->file($path,
             ['download' => true]
